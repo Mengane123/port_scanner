dd4e3eccb540b252487aad58240c17c62f09b6a8
prashant <prashantmengane007@gmail.com>
2024-05-30 18:06:36 +0530

    second commit

--- file.py
+++ file.py
@@ -1,1 +1,8 @@
 from scapy.all import *
+try:
+    host = input("Enter a host address: ")
+    l = list(input("Enter the port to scan: ")).split(",")
+    temp = map(int.p)
+    ports = list(temp)
+    
+    
