7af0ff21a53f10865ab1ede2d8ed721847b02c5f
prashant <prashantmengane007@gmail.com>
2024-05-31 13:20:48 +0530

    third commit

--- file.py
+++ file.py
@@ -2,7 +2,14 @@ from scapy.all import *
 try:
     host = input("Enter a host address: ")
     l = list(input("Enter the port to scan: ")).split(",")
-    temp = map(int.p)
+    temp = map(int.l)
     ports = list(temp)
+
+    if(re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",host)):
+        print("\n\nScanning...")
+        print("Host: ",host)
+        print("Ports: ",ports)
+
+        ans,unans = sr(If)
     
     
